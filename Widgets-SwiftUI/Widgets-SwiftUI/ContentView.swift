ceee602ab20c3ae4c648627aae8e30d80e8cfc96
Depdev25 <depdev25@gmail.com>
2022-09-21 22:49:20 -0500

    SwiftUI-Widgets: Initial commit

---
+++ Widgets-SwiftUI/Widgets-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Widgets-SwiftUI
+//
+//  Created by Gagandeep on 21/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Widgets-SwiftUI/Widgets-SwiftUI/Widgets_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widgets_SwiftUIApp.swift
+//  Widgets-SwiftUI
+//
+//  Created by Gagandeep on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Widgets_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
